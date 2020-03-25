75608492953f169371522a02c87f9ff820dced75
Lang-Wei <62399699+Lang-Wei@users.noreply.github.com>
2020-03-25 08:50:11 -0400

    Add files via upload

---
+++ Balloons_Lang.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#sorted balloon list
+x = [(114,136),(67,88),(14,126),(76,141),(66,78),(26,137),(85,133),(113,139),(43,141),(106,137),(60,128),(61,83),(57,102),(19,96),(43,99),(52,99),(41,102),(38,137),(71,108),(37,120)]
+balloon_sorted = sorted(x)
+print(balloon_sorted)
+N= len(x)
+print(N)
+
+ 
+
+throw_number=1
+dart=balloon_sorted[19][0]
+print (dart)
+
+ 
+
+for i in reversed(range(0,N)):
+    if dart > balloon_sorted[i][1]:
+        throw_number +=1
+        dart = balloon_sorted[i][0]
+        
+print(throw_number)
+print(dart)
+
